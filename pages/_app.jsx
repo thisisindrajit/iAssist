b988f96b278e987550c728f614e26aface92c9f9
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2022-06-11 15:51:16 +0530

    added initial dependencies

---
+++ pages/_app.jsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          name="description"
+          content="iAssist is a real-time chat support system for clearing students' doubts."
+        />
+        <meta
+          name="keywords"
+          content="iAssist, chat support, realtime, doubt clearance"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="p-6 text-lg">Welcome back, iAssist</div>
+    </div>
+  );
+}
