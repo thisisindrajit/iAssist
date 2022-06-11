588508d584f2ed0dada0eb398ac64858530a8fad
DhilipSanjay <53406309+DhilipSanjay@users.noreply.github.com>
2022-06-12 00:06:08 +0530

    Api completed

---
+++ pages/api/query/[qid]/updateQueryField.js
@@ -0,0 +1,26 @@
+import { firebase } from "/firebase/firebaseConfig";
+
+export const updateQueryField = async (qid, updatedFieldValue) => {
+    const queryRef = firebase.firestore()
+                        .collection("query")
+                        .doc(qid);
+    await queryRef.update(updatedFieldValue);
+};
+
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Only POST requests allowed" });
+    }
+
+    try{
+        const {qid} = req.query;
+        const updatedFieldValue = req.body;
+        await updateQueryField(qid, updatedFieldValue)
+        return res.status(200).json({ message: "Successfully updated Query Field" });
+    }
+    catch(error){
+        console.error("Error in updating query", error);
+        return res.status(500).json({ error: "Error in updating query" });
+    }
+    
+}
