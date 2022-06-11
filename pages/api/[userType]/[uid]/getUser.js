639a178b4812c75fc39ba5b85ef0b6a58f716d70
DhilipSanjay <53406309+DhilipSanjay@users.noreply.github.com>
2022-06-11 19:55:54 +0530

    User api

---
+++ pages/api/[userType]/[uid]/getUser.js
@@ -0,0 +1,22 @@
+import { firebase } from "/firebase/firebaseConfig";
+
+export const getUser = async (userType, uid) => {
+    const snapshot = await firebase.firestore()
+                        .collection(userType)
+                        .doc(uid).get();
+    if(snapshot.exists)
+        return snapshot.data();
+    else
+        return null;
+};
+
+export default async function handler(req, res) {
+    try{
+        const {userType, uid} = req.query;
+        return res.status(200).json(await getUser(userType, uid));
+    }
+    catch(error){
+        console.error("Error in query getUser ", error);
+        return null;
+    }
+}
